04e7f3d6f00d8f983c9d44c08896fdabb4fc2905
Pedro Lima <tsc.pedrolima@gmail.com>
2023-06-23 15:56:29 -0300

    style: add navbar mobile

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,24 +4,32 @@ import logo from '../../images/shared/logo.svg'
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg fixed-top navbar-light my-md-4 ml-md-5 pr-md-0" id='navbar'>
+    <nav className="navbar navbar-expand-md navbar-dark fixed-top m-3 my-md-4 ml-md-5 pr-md-0" id='navbar'>
         <Link className="navbar-brand" to="/"><img src={logo} alt="Logo" /></Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon custom-icon"></span>
+        <button 
+            className="navbar-toggler border-0 custom-toggle" 
+            type="button" 
+            data-toggle="collapse" 
+            data-target="#navbarSupportedContent" 
+            aria-controls="navbarSupportedContent" 
+            aria-expanded="false" 
+            aria-label="Toggle navigation"
+            >
+            <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse justify-content-md-end" id="navbarSupportedContent">
-            <ul className="nav p-md-3 px-md-5">
-                <li className="nav-item active">
-                    <Link className="nav-link font-primary-color font-secondary-family text-uppercase px-md-4" to="/">00 Home<span className="sr-only">(current)</span></Link>
+            <ul className="nav flex-column pl-2 py-5 flex-md-row p-md-3 px-md-5">
+                <li className="nav-item">
+                    <Link className="nav-link font-primary-color text-uppercase font-letter-space font-primary-color font-weight-light px-md-4" to="/"><span className='font-weight-bold'>00</span> Home<span className="sr-only">(current)</span></Link>
                 </li>
                 <li class="nav-item">
-                    <Link className="nav-link font-primary-color font-secondary-family text-uppercase px-md-4" to="/destination">01 Destination</Link>
+                    <Link className="nav-link font-primary-color font-primary-color font-letter-space text-uppercase px-md-4" to="/destination"><span className='font-weight-bold'>01</span> Destination</Link>
                 </li>
                 <li class="nav-item">
-                    <Link className="nav-link font-primary-color font-secondary-family text-uppercase px-md-4" to="/crew">02 Crew</Link>
+                    <Link className="nav-link font-primary-color font-primary-color font-letter-space text-uppercase px-md-4" to="/crew"><span className='font-weight-bold'>02</span> Crew</Link>
                 </li>
                 <li class="nav-item">
-                    <Link className="nav-link font-primary-color font-secondary-family text-uppercase px-md-4" to="/technology">03 Technology</Link>
+                    <Link className="nav-link font-primary-color font-primary-color font-letter-space text-uppercase px-md-4" to="/technology"><span className='font-weight-bold'>03</span> Technology</Link>
                 </li>
             </ul>
         </div>
